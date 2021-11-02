fc459824ba53a59db52acb36ed1b49e0332e77b8
Kenneth Fossen <fossen.kenneth@gmail.com>
2021-11-02 21:37:08 +0100

    new project

--- smartdogapp/lib/main.dart
+++ smartdogapp/lib/main.dart
@@ -1,36 +1,36 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:overlay_support/overlay_support.dart';
-import 'package:smartdogapp/notificationbadge.dart';
-import 'package:smartdogapp/pushnotification.dart';
-//import 'package:firebase_analytics/firebase_analytics.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return OverlaySupport.global(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-
-          primarySwatch: Colors.blue,
-        ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -44,87 +44,58 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final FirebaseMessaging _messaging;
-  PushNotification? _notificationInfo;
-
-
-
   int _counter = 0;
-
-  void onClick() {
-    // popup a toast.
-    toast('Hello world!');
-    _incrementCounter();
-    // show a notification at top of screen.
-    showSimpleNotification(
-        Text("this is a message from simple notification"),
-        background: Colors.green);
-  }
-
-  void registerNotification() async {
-    _messaging = FirebaseMessaging.instance;
-    NotificationSettings settings = await _messaging.requestPermission(
-      alert: true,
-      badge: true,
-      provisional: true,
-      sound: true,
-    );
-
-    if(settings.authorizationStatus == AuthorizationStatus.authorized) {
-      // handle notifications
-      FirebaseMessaging.onMessage.listen((RemoteMessage msg) {
-        print("Fetched a message");
-        PushNotification notification = PushNotification(
-          title: msg.notification?.title,
-          body: msg.notification?.body,
-        );
-        setState(() {
-          _notificationInfo = notification;
-          _counter++;
-        });
-      });
-
-      if (_notificationInfo != null) {
-        showSimpleNotification(
-          Text(_notificationInfo!.title!),
-          leading: NotificationBage(totalNotifications: _counter),
-          subtitle: Text(_notificationInfo!.body!),
-          background: Colors.green,
-          duration: Duration(seconds: 2),
-        );
-      }
-
-    } else {
-      print("Enable notifications");
-    }
-  }
 
   void _incrementCounter() {
     setState(() {
-
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
@@ -135,9 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: onClick,
+        onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
